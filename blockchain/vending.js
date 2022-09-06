9b60b4fa120f27a67a50dd86e0556614190f651d
Gizemdem <69429638+Gizemdem@users.noreply.github.com>
2022-09-06 19:03:23 +0200

    connection to wallet

---
+++ blockchain/vending.js
@@ -0,0 +1,12 @@
+import Web3 from 'web3'
+
+const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/472d9ff13ad6497da7e3b629eb283dea"
+)
+
+const web3 = new Web3(provider)
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
+const vmContract = new web3.eth.Contract(abi, "0x02976C0EC93CC8E201d2508cf2836448380b6Ce1")
+
+export default vmContract
